7f4194d46c179bc67dea47ef35dfbedbb174baeb
Green-m <greenm.xxoo@gmail.com>
2018-07-09 06:01:48 -0400

    Add config file and log file

--- sshnotify.sh
+++ sshnotify.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+config_file=/etc/sshnotify/whitelist.config
+log_file=/var/log/sshnotify/sshnotify.log
 
-white_user=(root)
-white_ip=(192.168.77.1)
+white_user=()
+white_ip=()
 res1=1
 res2=1
+
+if [[ -f $config_file ]]; then 
+  . $config_file
+fi
+
+function log_to_file() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S')  $@" >> $log_file
+}
 
 function emailsend() {
     local text="$1 login from $2 at $3"
     #sendEmail -f xxxxxxxxxxxxxxxxx@gmx.com -xu 'xxxxxxxxxx' -xp 'xxxxxxxxxxxxxxxxxx' -t xxxxxxxxxxxxxx@gmail.com -s mail.gmx.com:587 -u "SSH Login Warning" -m $text -o tls=yes -q
-    echo "Email sended"
+    log_to_file "Email sended"
 }
 
 #check_contain_arr 'xxx' arr[@]
@@ -19,33 +29,23 @@ function check_contain_arr () {
   return 0;
 }
 
-# function check_contain_hash () {
-#   local key value match="$1"
-#   shift
-#   for e; do [[ "$e" == "$match" ]] && return 0; done
-#   return 1
-# }
-
 while inotifywait -q -e modify /var/log/wtmp; do
  sleep 1
  read user ttyname ip start end <<< $(last -n 1 --time-format iso| awk 'NR==1{print $1,$2,$3,$4,$5}')
- #echo $user $ttyname $ip $start $end
+ log_to_file $user $ttyname $ip 
 
  # To reduce twice notify when login and logout,
  # only send mail when login.
  [[ $end != 'still' ]] && continue
 
  if [[ -n "$white_user" ]]; then
-    #echo $user ${white_user[@]}
     check_contain_arr $user white_user[@] 
     res1=$?
-    #echo "res1 is " $res1
  fi
 
  if [[ -n "$white_ip" ]]; then
     check_contain_arr $ip white_ip[@] 
     res2=$?
-    #echo "res2 is " $res2
  fi
 
  if [[ $res1 == 0 ]] || [[ $res2 == 0 ]]; then
